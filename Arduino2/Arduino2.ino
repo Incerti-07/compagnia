56de60df14fe592d5114b4ce6d7c9c206c70925e
INCERTI MATTEO <matteo.incerti.2007@calvino.edu.it>
2026-01-12 09:34:50 +0100

    inizio codice

--- Arduino2/Arduino2.ino
+++ Arduino2/Arduino2.ino
@@ -1,22 +1,21 @@
+#include "FastIMU.h"
 #include <Wire.h>
-#include <DFRobot_BMI160.h>
 
-// PIN sensori
+#define IMU_ADDRESS 0x68   // SDO collegato a GND
+
+MPU6050 IMU;
+
+calData calib = {0};
+AccelData accelData;
+GyroData gyroData;
+
+// PIN
 const int pinLM35 = A0;
 const int pinPIR  = 2;
+const int pinLED  = 6;
 
-// PIN LED
-const int pinLED = 6;
-
-// PIN interruttori
 const int pinSwitchTemp = 3;
 const int pinSwitchPIR  = 4;
-const int pinSwitchIMU  = 5;
-
-// Oggetto BMI160
-DFRobot_BMI160 bmi160;
-int16_t accel[3];
-int16_t gyro[3];
 
 // Timer
 unsigned long lastRead = 0;
@@ -24,27 +23,20 @@ const unsigned long interval = 3000;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {}
+  Wire.begin();
+  Wire.setClock(400000);
 
   pinMode(pinPIR, INPUT);
   pinMode(pinLED, OUTPUT);
 
   pinMode(pinSwitchTemp, INPUT_PULLUP);
   pinMode(pinSwitchPIR,  INPUT_PULLUP);
-  pinMode(pinSwitchIMU,  INPUT_PULLUP);
 
-  // I2C
-  Wire.begin();
-
-  // Inizializzazione BMI160
-  if (bmi160.softReset() != BMI160_OK) {
-    Serial.println("Errore reset BMI160");
-  }
-
-  if (bmi160.I2cInit(0x68) != BMI160_OK) {
-    Serial.println("BMI160 non trovato");
-  } else {
-    Serial.println("BMI160 pronto");
+  int err = IMU.init(calib, IMU_ADDRESS);
+  if (err != 0) {
+    Serial.print("Errore IMU: ");
+    Serial.println(err);
+    while (1);
   }
 
   Serial.println("Sistema avviato.");
@@ -54,19 +46,12 @@ void loop() {
 
   bool readTemp = (digitalRead(pinSwitchTemp) == LOW);
   bool readPIR  = (digitalRead(pinSwitchPIR)  == LOW);
-  bool readIMU  = (digitalRead(pinSwitchIMU)  == LOW);
-
-  // Nessun sensore attivo
-  if (!readTemp && !readPIR && !readIMU) {
-    digitalWrite(pinLED, LOW);
-    return;
-  }
 
   unsigned long now = millis();
   if (now - lastRead >= interval) {
     lastRead = now;
 
-    // --- LM35 ---
+    // ----- LM35 -----
     if (readTemp) {
       int raw = analogRead(pinLM35);
       float voltage = raw * 3.3 / 1023.0;
@@ -77,7 +62,7 @@ void loop() {
       Serial.println(" °C");
     }
 
-    // --- PIR ---
+    // ----- PIR -----
     if (readPIR) {
       int motion = digitalRead(pinPIR);
       Serial.print("Movimento: ");
@@ -91,20 +76,19 @@ void loop() {
       }
     }
 
-    // --- BMI160 IMU ---
-    if (readIMU) {
-      bmi160.getAccelData(accel);
-      bmi160.getGyroData(gyro);
+    // ----- BMI160 -----
+    IMU.update();
+    IMU.getAccel(&accelData);
+    IMU.getGyro(&gyroData);
 
-      Serial.println("BMI160 IMU:");
-      Serial.print("Accel X: "); Serial.print(accel[0]);
-      Serial.print(" Y: "); Serial.print(accel[1]);
-      Serial.print(" Z: "); Serial.println(accel[2]);
+    Serial.println("BMI160 IMU:");
+    Serial.print("Accel X: "); Serial.print(accelData.accelX);
+    Serial.print(" Y: "); Serial.print(accelData.accelY);
+    Serial.print(" Z: "); Serial.println(accelData.accelZ);
 
-      Serial.print("Gyro  X: "); Serial.print(gyro[0]);
-      Serial.print(" Y: "); Serial.print(gyro[1]);
-      Serial.print(" Z: "); Serial.println(gyro[2]);
-    }
+    Serial.print("Gyro  X: "); Serial.print(gyroData.gyroX);
+    Serial.print(" Y: "); Serial.print(gyroData.gyroY);
+    Serial.print(" Z: "); Serial.println(gyroData.gyroZ);
 
     Serial.println("-------------------------");
   }
